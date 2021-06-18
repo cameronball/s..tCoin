d696cbdfa2fe74928c40f5a2880ec3e1fd42ad2e
cameronball <49366907+cameronball@users.noreply.github.com>
2021-06-18 14:10:33 +0100

    Initial Upload

---
+++ shitCoin/miner_config.py
@@ -0,0 +1,13 @@
+"""Configure this file before you start mining. Check wallet.py for
+more details.
+"""
+
+# Write your generated adress here. All coins mined will go to this address
+MINER_ADDRESS = "WMKjio2dANGOBbMtsEoYiYb35e0V1IIe2Mlj2j5980R6V7pZ2mbK2c75VIjRI9e7yxr4VJVXyhgBVrdAHaJPkg=="
+
+# Write your node url or ip. If you are running it localhost use default
+MINER_NODE_URL = "http://localhost:5000"
+
+# Store the url data of every other node in the network
+# so that we can communicate with them
+PEER_NODES = []
